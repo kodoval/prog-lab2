9a5cbb5c9859d091f55d1ec841e46bb3163bee10
kodoval <bartanajhaan@gmail.com>
2026-05-22 09:28:11 +0700

    Удаление файлов не относящийся к заданию

--- task2_pangram.js
+++
@@ -1,8 +0,0 @@
-const readline = require("readline");
-const rl = readline.createInterface({ input: process.stdin });
- 
-rl.question("Введите строку: ", (s) => {
-    const unique = new Set(s.split("").filter(c => c >= 'a' && c <= 'z'));
-    console.log(unique.size === 26 ? "Yes" : "No");
-    rl.close();
-});
